3ea307951016ae0b0cd129866068d70a518d01b8
Eliana Ughetto <elianaughetto10@gmail.com>
2022-06-21 19:38:21 -0300

    deleted 'tp1-programacion' folder to readjusted

--- tercer-cuatri/Programacion 2/tp1-programacion/bootstrap/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register(function ($className) {
-	$filename = __DIR__ . "/../clases/" . $className . ".php";
-
-	if(file_exists($filename)) {
-		require_once $filename;
-	}
-});
--- tercer-cuatri/Programacion 2/tp1-programacion/clases/product.php
+++
@@ -1,9 +0,0 @@
-<?php 
-class Product {
-	public $product_id;
-	public $image;
-	public $image_description;
-	public $title;
-	public $short_description;
-	public $price;
-}
