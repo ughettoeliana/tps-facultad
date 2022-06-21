9c7c6b4e385a5db0ab133f46886deeedcde65ff8
Eliana Ughetto <elianaughetto10@gmail.com>
2022-06-21 19:40:54 -0300

    the files of js and manifest in tp1-programacion

--- tercer-cuatri/Aplicaciones web responsivas/tp1-potente/js/main.js
+++ tercer-cuatri/Aplicaciones web responsivas/tp1-potente/js/main.js
@@ -7,36 +7,36 @@ window.addEventListener("load", function () {
   }
 });
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
-    "X-RapidAPI-Key": "7f0d24d225mshaa26018bcd3e0dfp192af6jsn4943756234c3",
-  },
-};
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+//     "X-RapidAPI-Key": "7f0d24d225mshaa26018bcd3e0dfp192af6jsn4943756234c3",
+//   },
+// };
 
-fetch(
-  "https://online-movie-database.p.rapidapi.com/auto-complete?q=game",
-  options
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    const movieList = data.d;
+// fetch(
+//   "https://online-movie-database.p.rapidapi.com/auto-complete?q=game",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     const movieList = data.d;
 
-    movieList.map((item) => {
-      const title = item.l;
-      const img = item.i.imageUrl;
-      const id = item.id;
-      const button = `<button onclick="showDetails()" class="detail-a">Ver detalle</button>`;
-      const movie = `<div class='movies-container'><div class='movie'><img src="${img}"> <h2>${title}</h2> ${button}</div></div>`;
-      document.querySelector(".movies-section").innerHTML += movie;
-    });
-  })
+//     movieList.map((item) => {
+//       const title = item.l;
+//       const img = item.i.imageUrl;
+//       const id = item.id;
+//       const button = `<button onclick="showDetails()" class="detail-a">Ver detalle</button>`;
+//       const movie = `<div class='movies-container'><div class='movie'><img src="${img}"> <h2>${title}</h2> ${button}</div></div>`;
+//       document.querySelector(".movies-section").innerHTML += movie;
+//     });
+//   })
 
-  .then((response) => console.log(response))
+//   .then((response) => console.log(response))
 
-  .catch((err) => console.error(err));
+//   .catch((err) => console.error(err));
 
 function showDetails(id, img, title) {
   location.href = "/detail.html";
@@ -48,4 +48,11 @@ function sendComment() {
   const commentP = `<p>${inputValue}</p>`;
   document.querySelector(".show-comment-input").innerHTML += commentP;
   console.log(inputValue);
+}
+
+if (window.Notification && window.Notification.permission != 'denied') {
+setTimeout(() => {
+  Notification.requestPermission()
+  .then(res => console.log(res))
+}, 10000)
 }
--- tercer-cuatri/Aplicaciones web responsivas/tp1-potente/sw.js
+++ tercer-cuatri/Aplicaciones web responsivas/tp1-potente/sw.js
@@ -1,10 +1,10 @@
 const cacheName = 'Peliz.com';
 
 const assets = ['/',
-                '/index.html',
-                '/js/main.js',
-                '/styles.css'
-                ];
+    '/index.html',
+    '/js/main.js',
+    '/styles.css'
+];
 
 self.addEventListener('install', event => {
     self.skipWaiting();
@@ -27,18 +27,35 @@ self.addEventListener('fetch', event => {
                 }
                 let requestToCache = event.request.clone();
                 return fetch(requestToCache)
-                .then(res => {
-                    if (!res || res.status !== 200) {
+                    .then(res => {
+                        if (!res || res.status !== 200) {
+                            return res;
+                        }
+                        let responseToCache = res.clone();
+                        caches
+                            .open(cacheName)
+                            .then(cache => {
+                                cache.put(requestToCache, responseToCache)
+                            })
                         return res;
-                    }
-                    let responseToCache = res.clone();
-                    caches
-                    .open(cacheName)
-                    .then(cache => {
-                        cache.put(requestToCache, responseToCache)
                     })
-                    return res;
-                })
             })
     )
+})
+
+
+self.addEventListener('push', function (pushEvent) {
+    let title = pushEvent.data.text();
+    console.log(title);
+    let options = {
+        body: 'Nuevo lanzamiento de pelicula',
+        icon:'./assets/pochoclos-img.jpeg',
+        vibrate: [300,100,300],
+        data: {id:1},
+        actions: [
+            {'action': '1', 'title': 'Ver ahora', 'icon': './assets/portada-pelicula.jpeg'},
+            {'action': '2', 'title': 'Ver más tarde', 'icon': './assets/portada-pelicula.jpeg'}
+        ]
+    }
+    pushEvent.waitUntil(self.registration.showNotification(title, options));
 })
